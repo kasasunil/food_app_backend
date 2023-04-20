5ebd4509e71ebaf94d79222f05ae46d439df3591
kasasunil <kasasunil344@gmail.com>
2023-04-20 08:25:41 +0530

    Some routes added

--- app.js
+++ app.js
@@ -1,93 +1,159 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const mongoose = require('mongoose');
-const RecipeModel = require('./recipeModel');
-const UserModel = require('./userModel');
+const mongoose = require("mongoose");
+const RecipeModel = require("./recipeModel");
+const UserModel = require("./userModel");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/api/uploadRecipe', async(req, res) => {
-    try{
-        // for (let i = 0; i < arr.length; i++) {
-        //     console.log(arr[i].imageUrl);
-        // const Recipe = await RecipeModel.create(arr[i]);
-        // }
-        const Recipe = await RecipeModel.create(req.body);
-        console.log(Recipe);
-        return res.status(200).json({ message: "Recipe uploaded successfully" });
-    }
-    catch(err){
-        console.log(err);
-        return res.status(500).json({ message: "Error uploading recipe" });
-    }
+app.post("/api/uploadRecipe", async (req, res) => {
+  try {
+    // for (let i = 0; i < arr.length; i++) {
+    //     console.log(arr[i].imageUrl);
+    // const Recipe = await RecipeModel.create(arr[i]);
+    // }
+    const img = {
+      //   "Chicken Curry":
+      //     "https://lh3.googleusercontent.com/u/0/drive-viewer/AAOQEOStyo_7nu2gReuqPfmd3ybKAvOw32IqRCrg_wpZYbUl8j2Q7LWxNDzaAy95sefaV0bU9wF_7e86Pso0APaQxPOWPuYHiw=w1920-h892",
+      //   "Fish Curry":
+      //     "https://lh3.googleusercontent.com/u/0/drive-viewer/AAOQEOSbw4hL_gnxJMZSV3-0DlRlhMOfPUWb0z1knQ9sbIaAz76GJHnBVcy_BtDeJjFhG-w0vVps5FnHhYicTrSbNj2qRuJ4=w958-h880",
+      //   "Butter Chicken":
+      //     "https://lh3.googleusercontent.com/u/0/drive-viewer/AAOQEOStAhsnYTFPsDf-oQ81SvgP5HBtStIxtUBSXTnTrXTYpJEq76Q39U7t5jbmpnnqpVedZ202Y5rB_n5ApH_SViV7jNH-nA=w958-h880",
+      "Mutton Curry":
+        "https://lh3.googleusercontent.com/u/0/drive-viewer/AAOQEOTYyoqXk_IHjB-KqTm6Dgp68TA__l_V4xz_9dCLnp706G7o2a-blGfS9dNmq2cOjCBlfjlwCpjW2uSdyEbzLanak7n5hQ=w958-h880",
+      //   "Gongura Chicken":
+      //     "https://lh3.googleusercontent.com/u/0/drive-viewer/AAOQEOQEVRCvmMfehO932xkxZCgA_r0c2Rjc9-LOV23aQhed58zRDZ2fJHBnil8VrNi2TxxHvcP-eaQyNJl4RDzO6EezIDxJlw=w958-h880",
+      //   "Special Dry Chicken":
+      //     "https://lh3.googleusercontent.com/u/0/drive-viewer/AAOQEOR7B5uzmNe8TL66neULupE3Wn-K1uqwZfVv2AI1u3iws7Eu5udoVrPQyOqbSCEpFLfCPXKU7SQveY5RQc-zlh72u1pD=w958-h880",
+      //   "Pepper Chicken":
+      //     "https://lh3.googleusercontent.com/u/0/drive-viewer/AAOQEOTJbXc9olXV-9TXxuE3oT10MQbaak0xzelAZdqYFm_WeM7rAfTWW9zyXi2XSP6RVWHZ3j0JjZHj2bE4VpaP0ItGSFxCLA=w958-h880",
+      //   "Mughalai Mutton Gravy":
+      //     "https://lh3.googleusercontent.com/u/0/drive-viewer/AAOQEOTcT_qgW1MWyMH2ljz_K91GL7IngXCXclHM7iOn5P62XDs1rB7KIm-MuPl_u_PdmVSGcY2yeklKmvnhpdEc5cjHI2c4HQ=w958-h880",
+      //   "Hyderabadi Chicken Gravy":
+      //     "https://lh3.googleusercontent.com/u/0/drive-viewer/AAOQEORcXfFFYMjq2WGNk5ZJotrjAoo24BSF_Qw8bowgbWnisln22_hV17xsHYfi7v-ajF4SWkqYP0Kc0EC-1LRDQdMPo_LOLg=w958-h880",
+      //   "Malabar Chicken Curry":
+      //     "https://lh3.googleusercontent.com/u/0/drive-viewer/AAOQEOQ0Zdp1CkbANn2yzdDjxMxlvuIbz32xHS3fZaQp8PodvdAVtNx7l6GzWK1s0Zv1U8ZlYMZ22lJjaH-dfQr_IfUrZB_pMw=w958-h880",
+      //   "Chilli Chicken":
+      //     "https://lh3.googleusercontent.com/u/0/drive-viewer/AAOQEOSCjxwhaEbZekqkh5KQPteGkJkSjNf4HeHiEVUsv8MIJp0O4t7Ik_NHSfsERfpkpaVK_PiJ2yV6ZzHbkU7HLfxMMjtf5A=w958-h880",
+      //   "Kashmir Chicken Gravy":
+      // "https://lh3.googleusercontent.com/u/0/drive-viewer/AAOQEOSNJHuVufNXpqPisToQ4486TmVXMClS1jA3pJbc9wxdU787IFuou1jdAq0sT0LRGDBzwTDYBJjom4FBsUk98tUjN0Vjuw=w958-h880",
+    };
+    const Recipe = await RecipeModel.create(req.body);
+    console.log(Recipe);
+    // var c=0;
+    // for(let key of Object.keys(img)){
+    //     const result = await RecipeModel.findOneAndUpdate(
+    //       { name: key },
+    //       { imageUrl: img[key] },
+    //       { new: true, upsert: true,runValidators: true ,findAndModify: false }
+    //     );
+    //     console.log(key,c++);
+    // }
+    return res.status(200).json({ message: "Recipe uploaded successfully" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Error uploading recipe" });
+  }
 });
 
 app.put("/api/addReview", async (req, res) => {
-    const {recipe, review} = req.body;
-    const post = await RecipeModel.findOneAndUpdate({name: recipe}, {$push: {reviews: review}});
+  try {
+    const { recipe, review } = req.body;
+    const post = await RecipeModel.findOneAndUpdate(
+      { name: recipe },
+      { $push: { reviews: review } }
+    );
     console.log(post);
     return res.status(200).json({ message: "Review added successfully" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Error adding review" });
+  }
 });
 
 app.post("/api/addUser", async (req, res) => {
-    const {name, email} = req.body;
-    const user = await UserModel.create({name, email});
+  try {
+    const { name, email } = req.body;
+    const user = await UserModel.create({ name, email });
     console.log(user);
     return res.status(200).json({ message: "User added successfully" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Error adding user" });
+  }
 });
 
 app.get("/api/allRecipes", async (req, res) => {
+  try {
     const recipes = await RecipeModel.find();
     console.log(recipes);
     return res.status(200).json(recipes);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Error adding user" });
+  }
 });
 
 app.post("/api/addToCart", async (req, res) => {
-    const {recipe, email} = req.body;
+  try {
+    const { recipe, email } = req.body;
     const post = await UserModel.findOneAndUpdate(
       { email: email },
       { $push: { cartItems: recipe } },
-      { new: true ,upsert: true ,runValidators: true ,findAndModify: false }
+      { new: true, upsert: true, runValidators: true, findAndModify: false }
     );
     console.log(post);
-    return res.status(200).json({ message: "User added successfully" });
-})
+    return res.status(200).json({ message: "Added to cart successfully" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Error adding to cart" });
+  }
+});
 
 app.post("/api/addToFavorite", async (req, res) => {
-    const {recipe, email} = req.body;
+  try {
+    const { recipe, email } = req.body;
     const post = await UserModel.findOneAndUpdate(
       { email: email },
       { $push: { favoriteItems: recipe } },
-      { new: true ,upsert: true ,runValidators: true ,findAndModify: false }
+      { new: true, upsert: true, runValidators: true, findAndModify: false }
     );
     console.log(post);
-    return res.status(200).json({ message: "User added successfully" });
-})
+    return res.status(200).json({ message: "Added to favorites successfully" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Error adding to favorites" });
+  }
+});
 
 app.get("/api/getCartItems/:email", async (req, res) => {
-    const email=req.params.email;
-    const user = await UserModel.findOne({email: email});
+  try {
+    const email = req.params.email;
+    const user = await UserModel.findOne({ email: email });
     const cartItems = user.cartItems;
-    const arr=[];
+    const arr = [];
     for (let i = 0; i < cartItems.length; i++) {
-        const recipe = await RecipeModel.findOne({name: cartItems[i]});
-        arr.push(recipe);
+      const recipe = await RecipeModel.findOne({ name: cartItems[i] });
+      arr.push(recipe);
     }
     return res.status(200).json(arr);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Error adding to favorites" });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-    mongoose.connect(
-      "mongodb+srv://Sunil:sunil@atlascluster.tvkddrz.mongodb.net/test",
-      { useNewUrlParser: true, useUnifiedTopology: true }
-    );
-    const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', function() {
-        console.log('Connected to MongoDB');
-    });
+  console.log(`Server is running on port: ${port}`);
+  mongoose.connect(
+    "mongodb+srv://Sunil:sunil@atlascluster.tvkddrz.mongodb.net/test",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  );
+  const db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error:"));
+  db.once("open", function () {
+    console.log("Connected to MongoDB");
+  });
 });
-
